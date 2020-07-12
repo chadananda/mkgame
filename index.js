cdec3fa49b299a038c556016bd9befe837558aeb
Chad <chadananda@gmail.com>
2020-07-12 16:28:48 -0700

    basically working

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 #!/usr/bin/env node
 
 /****************************
-This was an afternoon project I wrote to help my 7-yr-old learn TensorFlow.js
+This was an afternoon project I wrote to help my 7-yr-old learn PhaserJS
 
-Why? Gathering up training images is a pain. With this CLI command, you can gather
-thousands of training images in a minute, organized into folders by keyword.
+Why? Too much boilerplate to get in the way of learning the fun stuff.
 
-Just install globally and then, from inside any directory invoke like:
+Also, the darned PhaserJS website is awful -- they actually advise installing MAMP.
+What is this the 80's?
 
- > ts-images "dog, cat, mouse, rat"
-
-A `ts-images` dir will be created with sub-directories `dog`, `cat` etc.
-Each sub-directory will contain hundreds of keyword images.
+I found an excellent starter template and adjusted it to my needs then created this
+little installer to do the initial setup.
 **************************/
 
-const axios = require('axios')
 const chalk = require('chalk')
 const clear = require('clear')
 const figlet = require('figlet')
-const path = require('path')
+// const path = require('path')
 const fs = require('fs')
-const md5 = require('md5')
 const inquirer = require('inquirer')
-const Scraper = require('images-scraper')
-const cwd = process.cwd()
+// const cwd = process.cwd()
 const args = process.argv
-const MAX_PER_KW = 1000
+const gitUser = require('git-user-name')
+const gitUsername = require('git-username')
+const gitEmail = require('git-user-email')
+const { execSync } = require('child_process')
+const open = require('open')
+const readline = require('readline')
+const template = 'https://github.com/chadananda/phaser-auto-template'
 
-var images_dir = '' //makeNewImagesDir()
-var total_image_count = 0
+var replacements = {
+  game_title:'', project_name:'', game_description:'', author_name:'', author_email:'', game_domain:'', gitUser:''
+}
 
-// suppress unwanted messages from other modules
-console.info=(()=>{})
 
 /******************************/
 init()
@@ -41,104 +41,203 @@ async function init() {
   clear()
   console.log(
     chalk.green.bold.italic(
-      figlet.textSync('TF-Images', { horizontalLayout: 'full', font: 'Fire Font-s' })
+      figlet.textSync('M K G A M E', { horizontalLayout: 'full', font: 'Fire Font-s' })
     )
   )
-  console.log(chalk.gray.bold(`\nTensorFlow training needs lots of keyword images. This little tool fetches tons of images from Google image search and drops then into keyword-named folders as a starting point. Make sure to browse the folers and remove inappropriate images before starting your ML training.`))
+  console.log(chalk.gray.bold(`\n Boilerplate is tedious. Let's make a new PhaserJS game!  👍👍`))
 
-  // get keywords from arguments or request from command prompt
-  let keywords = getKeywords()
-  if (!keywords || keywords.length<1) {
-    let kw = await promptForKeywords()
-    keywords = getKeywords(kw.kw)
+
+  let input, def
+
+  replacements.author_name = gitUser()
+  replacements.author_email = gitEmail()
+  replacements.gitUser = gitUsername()
+
+  // Game Title
+  def = args.slice(2)[0] || ''
+  replacements.game_title = def
+  if (!def || def.length<1) {
+    input = await promptForTitle()
+    replacements.game_title = input.title.trim()
   }
-  console.log('')
 
+  // Project Name
+  def = replacements.game_title.toLowerCase().trim().replace(/ /g, '-')
+  input = await promptForProjectName(def)
+  replacements.project_name = input.proj.trim()
 
-  if (keywords.length>0) {
-    images_dir = makeNewImagesDir()
-    getKeywordImages(keywords).then(() => {
-      console.log(
-        chalk.green(`🥳 ${total_image_count} images downloaded to: `),
-        chalk.green.bold('/' + images_dir.split('/').slice(-2).join('/'), ` 🥳 🥳 🥳 \n\n\n`)
-      )
-      process.exit()
-    })
-  } else {
-    console.error('Sorry, cannot fetch keyword images without some keywords!')
+  if (fs.existsSync(replacements.project_name)) {
+    console.log('\n\n')
+    console.warn(chalk.red.bold(' ⚠️  WARNING: ') + ' cannot continue, folder already exists: '+ chalk.red(replacements.project_name))
+    console.log('\n\n')
     process.exit()
   }
-}
+
+  // Game Description
+  input = await promptForDescription()
+  replacements.game_description = input.desc.trim()
+
+  // Public Game URL
+  def = `${replacements.gitUser.toLowerCase()}.github.io/${replacements.project_name}`
+  input = await promptForURL(def)
+  replacements.game_domain = input.url.trim()
+  replacements.git_repo = `git@github.com:${replacements.gitUser}/${replacements.project_name}.git`
+
+  await confirm(
+    chalk.red.bold('Hit enter to create a repository on GitHub (cloned from a template). Use "')+
+    chalk.green.bold(replacements.project_name)+
+    chalk.red.bold('" as your new repository name.')
+  )
+
+  await open( `${template}/generate`)
+
+  await confirm(
+    chalk.red.bold('Hit enter when GitHub has finished creading your new repository "')+
+    chalk.green.bold(replacements.project_name)+
+    chalk.red.bold('"')
+  )
+
+  await generateProject(replacements)
+
+  process.exit()
+
+/*
+  details:
+    github_user
+
+
+  replacements:
+
+  [[game_title]]
+  [[project_name]]
+  [[game_description]]
+  [[author_name]]
+  [[author_email]]
+  [[game_domain]]
+
+
+*/
 
 
 
-async function promptForKeywords() {
-  console.log()
-  return inquirer.prompt([{
-    message: "Comma-delimited list of keywords (or phrases): ",
-    name: "kw"    /* Pass your questions in here */
-  }])
-}
 
 
+  async function promptForTitle() {
+    console.log('')
+    return inquirer.prompt([{
+      message: "🤖 Game Title: ",
+      name: "title"    /* Pass your questions in here */
+    }])
+  }
 
-function getKeywords(kw) {
-  if (!kw) kw = args.slice(2)[0] || ''
-  return kw.trim().split(',')
-           .map(kw => kw.trim().toLowerCase()).filter(kw=>kw)
-           .filter((kw,i,ls) => ls.indexOf(kw)===i)
-}
+  async function promptForProjectName(title) {
+    console.log('')
+    return inquirer.prompt([{
+      message: "🤖 Repository Name: ",
+      name: "proj",    /* Pass your questions in here */
+      default: title
+    }])
+  }
+
+  async function promptForDescription() {
+    console.log('')
+    return inquirer.prompt([{
+      message: "🤖 Project Description: ",
+      name: "desc",    /* Pass your questions in here */
+    }])
+  }
+
+  async function confirm(message) {
+    console.log()
+    const rl = readline.createInterface({ input: process.stdin, output: process.stdout, })
+    return new Promise(resolve => rl.question(message, ans => {
+      rl.close()
+      resolve(ans)
+    }))
+  }
+
+  async function promptForURL(def) {
+    console.log('')
+    return inquirer.prompt([{
+      message: "🤖 Public Game URL (you can change this later): ",
+      name: "url",    /* Pass your questions in here */
+      default: def
+    }])
+  }
 
 
-async function getKeywordImages(kwlist) {
-  // return new Promise((resolve, reject) => {
-    let allRequests = []
-    for (const kw of kwlist) {
-    // kwlist.forEach((kw) => {
-      let targetDir = `${images_dir}/${kw.replace(/ /g, '-')}`
-      fs.mkdirSync(targetDir)
-      // console.log.clearLine(); console.log.cursorTo(0)
-      console.log(chalk.gray.bold('\n🤖 Searching Google for images by keyword: "'+kw+'"'))
-      const scraper = new Scraper({
-        userAgent: 'Mozilla/5.0 (X11; Linux i686; rv:64.0) Gecko/20100101 Firefox/64.0',
-        puppeteer: { headless: true, },
-        tbs: {  // every possible tbs search option, some examples and more info: http://jwebnet.net/advancedgooglesearch.html
-          isz: "m", // options: l(arge), m(edium), i(cons), etc.
-          itp:  "photo",// options: clipart, face, lineart, news, photo
-          ic:   "color",// options: color, gray, trans
-          sur:  ""// options: fmc (commercial reuse with modification), fc (commercial reuse), fm (noncommercial reuse with modification), f (noncommercial reuse)
-        },
-      })
-      let results = await scraper.scrape(kw, MAX_PER_KW)
-      if (results) results.forEach( ({url}) => {
-        let ext = path.extname(url).split('?')[0].toLowerCase()
-        if (['.jpg','.jpeg','.png'].indexOf(ext)>-1) {
-          let out = `${targetDir}/${md5(url)}${path.extname(url).split('?')[0]}`
-          let method = "get", responseType = "stream"
-          allRequests.push( axios({method, url, responseType}).then( (res, err) => {
-            res.data.pipe(fs.createWriteStream(out))
-            total_image_count++
-           }).catch((err)=>{  })
-          )
-        }
-      })
-      else console.log('No scraper results...')
-      // console.log(requests)
+ async function replaceStuff(tdir, replacements) {
+   let files = ['README.md', 'package.json', 'public/index.html', 'public/manifest.json', 'src/index.js' ]
+   let targets = []
+   files.forEach(file => {
+     file = `${tdir}/${file}`
+     if (fs.existsSync(file)) {
+       let text = fs.readFileSync(file, "utf8")
+       if (text.length) Object.keys(replacements).forEach(key => {
+         text = text.replace(new RegExp(`\\[\\[${key}\\]\\]`, 'g'), replacements[key])
+       })
+       fs.writeFileSync(file, text)
+     }
+   })
+ }
+
+
+ async function generateProject(replacements) {
+    let r = replacements // because the word is just too long
+    let tdir = process.cwd() +'/'+ r.project_name
+    // console.log('Generating repo: '+r.project_name, 'at', `${template}/generate`, )
+
+    // create and cd to project dir
+    console.log(chalk.green.bold('\n ✅ ') + chalk.gray('Creating project folder: '+r.project_name))
+    if (!fs.existsSync(tdir)) fs.mkdirSync(tdir)
+
+    // clone repo
+    console.log(chalk.green.bold('\n ✅ ') + chalk.gray('Initializing git from template: '+template))
+    execSync(`git clone -q '${replacements.git_repo}' '${tdir}'`)
+
+    // update files
+    console.log(chalk.green.bold('\n ✅ ') + chalk.gray('Customizing template files and pushing changes to GitHub...'))
+    await replaceStuff(tdir, replacements)
+    process.chdir(tdir)
+    execSync('git commit -am "customized template with search and replace"')
+    execSync('git push')
+
+    // add CNAME file if needed
+    let def_domain = `${replacements.gitUser.toLowerCase()}.github.io/${replacements.project_name}`
+    if (replacements.game_domain!=def_domain) {
+      console.log(chalk.green.bold('\n ✅ ') + chalk.gray('Adding custom domain CNAME file...'))
+      let output = tdir +'/public/CNAME'
+      fs.writeFileSync(output, replacements.game_domain)
+      // add file to GIT
+      let cmd = 'git add '
+      execSync('git add ./public/CNAME')
+      execSync('git commit -am "added custom CNAME file to /public dir"')
+      execSync('git push')
     }
-    // done gathering requests
-    // console.log.clearLine(); console.log.cursorTo(0)
-    console.log(chalk.gray.bold('\n👍 Downloading '+ allRequests.length +' images...\n'))
-    return Promise.all(allRequests)
-  // })
+
+    // build and deploy
+    console.log(chalk.green.bold('\n ✅ ') + chalk.gray('Installing NPM modules, this can take a while!'))
+    execSync('npm install')
+    execSync('git add package-lock.json')
+    execSync('git commit -am "added package-lock.json to repo"')
+    execSync('git push')
+
+    // build
+    console.log(chalk.green.bold('\n ✅ ') + chalk.gray('Building game...'))
+    execSync('npm run build')
+
+    // build
+    console.log(chalk.green.bold('\n ✅ ') + chalk.gray('Deploying game to GitHub, you can view at: http://'+r.game_domain+'\n\n\n'))
+    execSync('npm run deploy')
+
+    console.log('🥳 🥳 🥳 \n\n\n')
+
+  }
+
 }
 
-function makeNewImagesDir() {
-  let target = `${cwd}/tf-images`
-  let iterator = 1
-  while (directoryExists(target)) target = `${cwd}/tf-images_${iterator++}`
-  fs.mkdirSync(target)
-  return target
-}
+
+
 
 
 
